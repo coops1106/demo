f165564daf387da331ae65979fb5de3a367d8435
lv14942 <dave.cooper@lv.com>
2015-04-27 10:34:57 +0100

    Simple PRG

--- src/main/java/demo/GreetingController.java
+++ src/main/java/demo/GreetingController.java
@@ -3,14 +3,25 @@ package demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+    @RequestMapping(value = "/")
+    public String getDefault() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/greeting", method = {RequestMethod.GET})
+    public String greetingGet(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greetingPage";
+    }
+
+    @RequestMapping(value = "/greeting", method = {RequestMethod.POST})
+    public String greetingPost(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        return "redirect:greeting?name=" + name;
     }
 }
--- src/test/java/demo/GreetingControllerTest.java
+++ src/test/java/demo/GreetingControllerTest.java
@@ -3,11 +3,15 @@ package demo;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class GreetingControllerTest {
 
@@ -21,7 +25,12 @@ public class GreetingControllerTest {
     }
 
     @Test
-    public void testGreeting() throws Exception {
-        mockMvc.perform(get("/greeting?name=\"Exeter\"")).andDo(print());
+    public void testGreetingGet() throws Exception {
+        mockMvc.perform(get("/greeting?name=\"Exeter\"")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGreetingPost() throws Exception {
+        mockMvc.perform(post("/greeting?name=\"Exeter\"")).andDo(print());
     }
 }
